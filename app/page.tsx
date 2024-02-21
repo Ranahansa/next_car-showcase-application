799df6fcd0b2cf8e9fba8027ef904c69abb7c5d1
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-02-21 12:59:15 +0530

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/Hero";
 import NextImage from "next/image";
 
 export default function HomePage() {
   return (
     <main className="overflow-hidden">
-      <div>Hello, World <span role="img" aria-label="blush">😊</span></div>
+      <Hero />
     </main>
   );
 }
