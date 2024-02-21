67b6f16c1d492f9ac1f793b32ae307077cb6605f
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-02-21 12:28:26 +0530

    commit 1

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import NextImage from "next/image";
+
+export default function HomePage() {
+  return (
+    <main className="overflow-hidden">
+      <div>Hello, World <span role="img" aria-label="blush">😊</span></div>
+    </main>
+  );
+}
