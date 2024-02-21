16845200eab2904a9458301ced3c1e77e1bfea89
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-02-21 13:51:12 +0530

    commit 3

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,36 @@
+"use client"
 import React from 'react'
+import Button from './Button'
+import Image from 'next/image'
 
 const Hero = () => {
+
+    const handleScroll = () => {
+        
+    }
     return (
-        <div>
-            hi
+        <div className='hero'>
+            <div className='flex-1 pt-36 padding-x'>
+                <h1 className='hero__title'>
+                    Find , Book or Rent a car - quick and easily!
+                </h1>
+                <p className='hero__subtitle'>
+                    Streamline your car rental experience with our effortless booking process. Whether you’re traveling for business or leisure, we’ll make sure you have a hassle-free experience. 
+                </p>
+                <Button 
+                    title = "Explore Cars"
+                    containerStyles = "bg-primary-blue text-white rounded-full mt-10"
+                    handleClick = { handleScroll }
+                />
+            </div>
+            <div className='hero__image-container'>
+                <div className='hero__image'>
+                    <Image src="/hero.png" alt = "hero" fill className='object-contain'/>
+                    <div
+                        className='hero__image-overlay'
+                    />
+                </div>
+            </div>
         </div>
     )
 }
