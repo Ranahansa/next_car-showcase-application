00e3d5988d23e07288e92e4a4d62f42a9d7ffd5a
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-02-22 15:29:28 +0530

    commit 5

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import Button from './Button';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
-    )
+        <header className="w-full absolute z-10">
+            <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+                <Link href={"/"} className="flex justify-center items-center">
+                    <Image
+                        src="https://static.vecteezy.com/system/resources/previews/013/923/543/original/blue-car-logo-png.png"
+                        alt="logo"
+                        width={78}
+                        height={18}
+                        className="object-contain"
+                    />
+                    <span className="text-black text-[18px] font-bold ml-1 font-mono">
+                        .Hub
+                    </span>
+                </Link>
+                <Button
+                    title="Sign In"
+                    btnType="button"
+                    containerStyles="text-black font-bold rounded-lg bg-white border-2 border-black min-w-[80px] h-9 flex justify-center items-center"
+                />
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar
