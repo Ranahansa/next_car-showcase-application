a3e2d210e6c7bd515209dd14c3609c136a8760f8
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-03-09 01:43:42 +0530

    commit 10

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,18 +1,50 @@
 "use client"
-import { Combobox,Transition } from '@headlessui/react'
+import { Combobox, Transition } from '@headlessui/react'
 import { SearchManufacturerProps } from '@/types'
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import Image from 'next/image'
+import { manufacturers } from '@/constants'
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
 
-const SearchManufacturer = ({manufacturer, setManufacturer}:SearchManufacturerProps) => {
+    const [query, setQuery] = useState('')
+    const filteredManufacturers = query === '' ? manufacturers : manufacturers.filter((item) => (
+        item.toLowerCase().replace(/\s/g, '')
+        .includes(query.toLowerCase().replace(/\s/g, ''))
+    ))
+
     return (
-        <div className='search-manufacturer'>
+        <div className="search-manufacturer">
             <Combobox>
-                <div className='relative w-full'>
-                    
+                <div className="relative w-full">
+                    <Combobox.Button className="absolute top-[14px]">
+                        <Image
+                            src="https://static.vecteezy.com/system/resources/previews/013/923/543/original/blue-car-logo-png.png"
+                            width={20}
+                            height={20}
+                            className="ml-4"
+                            alt="logo"
+                        />
+                    </Combobox.Button>
+                    <Combobox.Input
+                        className="search-manufacturer__input"
+                        placeholder="Land Rover"
+                        displayValue={(manufacturer: string) => manufacturer}
+                        onChange={(e) => {
+                            setQuery(e.target.value);
+                        }}
+                    />
+                    <Transition 
+                    as={Fragment}
+                        leave= "transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        afterLeave={() => setQuery('')}
+                    >
+                    </Transition>
                 </div>
             </Combobox>
         </div>
-    )
+    );
 }
 
-export default SearchManufacturer
+export default SearchManufacturer 
