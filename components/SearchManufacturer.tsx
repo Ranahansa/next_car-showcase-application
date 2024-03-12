4453583fe1ee540ddfacdcde76e5f5b37ce7e4e0
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-03-12 22:35:22 +0530

    commit 13

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -9,12 +9,12 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
     const [query, setQuery] = useState('')
     const filteredManufacturers = query === '' ? manufacturers : manufacturers.filter((item) => (
         item.toLowerCase().replace(/\s+/g, '')
-        .includes(query.toLowerCase().replace(/\s+/g, ''))
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
     ))
 
     return (
         <div className="search-manufacturer">
-            <Combobox>
+            <Combobox value = {manufacturer} onChange={setManufacturer}>
                 <div className="relative w-full">
                     <Combobox.Button className="absolute top-[14px]">
                         <Image
@@ -33,24 +33,42 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                             setQuery(e.target.value);
                         }}
                     />
-                    <Transition 
-                    as={Fragment}
-                        leave= "transition ease-in duration-100"
+                    <Transition
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
-                        afterLeave={() => setQuery('')}
+                        afterLeave={() => setQuery("")}
                     >
                         <Combobox.Options>
-                            {filteredManufacturers.length === 0 && query !== "" ? (
+                            {filteredManufacturers.map((item) => (
                                 <Combobox.Option
-                                    value={query}
-                                    className="search-manufacturer__option"
+                                    key={item}
+                                    className={({ active }) =>
+                                        `relative search-manufacturer__option ${active ? "bg-primary-blue text-white" : "text-gray-900"
+                                        }`
+                                    }
+                                    value={item}
                                 >
-                                    Create "{query}"
+                                    {({ selected, active }) => (
+                                        <>
+                                            <span
+                                                className={`block truncate ${selected ? "font-medium" : "font-normal"
+                                                    }`}
+                                            >
+                                                {item}
+                                            </span>
+                                            {selected ? (
+                                                <span
+                                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? "text-white" : "text-teal-600"
+                                                        }`}
+                                                >
+                                                </span>
+                                            ) : null}
+                                        </>
+                                    )}
                                 </Combobox.Option>
-                            ):(
-                                
-                            )}
+                            ))}
                         </Combobox.Options>
                     </Transition>
                 </div>
