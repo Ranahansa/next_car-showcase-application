9ebf37e93781b3276070cdd55bd50088d0844a15
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-03-17 00:18:15 +0530

    commit 16

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -7,9 +7,16 @@ interface CarCardProps {
     car: CarProps;
 }
 const CarCard = ({car} : CarCardProps) => {
+
+    const {city_mpg, year, make, model, transmission, drive} = car;
+
     return (
-        <div>
-            
+        <div className='car-card group'>
+            <div className='car-card__content'>
+                <h2 className='car-card__content-title'>
+                    {make} {model}
+                </h2>
+            </div>
         </div>
     )
 }
